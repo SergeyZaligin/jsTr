3a43a4913989bdcf3c3d202efef638a92959b77a
sergey <sergey>
2018-08-10 13:01:14 +0300

    fix

--- js/truck.js
+++ js/truck.js
@@ -6,18 +6,25 @@
 	function Truck(truckId, db) {
 		this.truckId = truckId;
 		this.db = db;
-	}
+	};
 
 	Truck.prototype.createOrder = function (order) {
 		console.log('Adding order for ' + order.emailAddress);
 		this.db.add(order.emailAddress, order); 
-	}
+	};
 
 	Truck.prototype.deliverOrder = function (customerId) {
 		console.log('Delivering order for ' + customerId);
 		this.db.remove(customerId);
 	};
-	
+
+	Truck.prototype.printOrders = function () {
+		const orders = Object.keys(this.db.getAll());
+		orders.forEach((email) => {
+			console.log('This order', this.db.get(email));
+		});
+	};
+
 	App.Truck = Truck;
 	global.App = App;
 
