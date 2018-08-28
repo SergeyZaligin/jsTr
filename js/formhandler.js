85a2a2bd3f7e37c4c130f658fc113d356c1f3938
sergey <sergey>
2018-08-28 15:00:58 +0300

    fix

--- js/formhandler.js
+++ js/formhandler.js
@@ -1,12 +1,15 @@
 (function (global) {
   'use strict';
   var App = global.App || {};
+  var $ = global.jQuery;
 
   function FormHandler() {
-
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
   }
 
   App.FormHandler = FormHandler;
   global.App = App;
-  
+
 })(window);
