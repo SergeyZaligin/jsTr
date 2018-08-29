b19d5a088ba6d058ee06b532e38c1cf2f4399620
sergey <sergey>
2018-08-29 13:07:12 +0300

    fix

--- js/formhandler.js
+++ js/formhandler.js
@@ -3,12 +3,20 @@
   var App = global.App || {};
   var $ = global.jQuery;
 
-  function FormHandler() {
+  function FormHandler(selector) {
     if (!selector) {
       throw new Error('No selector provided');
     }
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
   }
-
+  FormHandler.prototype.addSubmitHandler = function () {
+    this.$formElement.on('submit', function (event) {
+      event.preventDefault();
+    });
+  };
   App.FormHandler = FormHandler;
   global.App = App;
 
